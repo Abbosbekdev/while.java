8d4c0f377fc31d3983f7782dcb20feeb76898cf8
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-07 10:13:27 +0500

    Create while.java
    java dasturlash tilidan while loop

---
+++ while.java
@@ -0,0 +1,103 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class tsik_while_dowhile {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+        System.out.print("a = ");
+        int a =scanf.nextInt();
+        System.out.print("b = ");
+        int b = scanf.nextInt(),i=0,kopaytir=1,yigindi=0;
+        /*
+     //1-masala
+        while (a-b >0){
+            System.out.println(a);
+            --a;
+        }
+     //2-masala
+           while (a>=b){
+               a=a-b;
+               ++i;
+           }
+        System.out.println(i+" ta kesma joylashsa bo`ladi");
+     //3-masala
+         while (a>=b){
+               a=a-b;
+               ++i;
+           }
+        System.out.println("bo'linma : "+i);
+        System.out.println("qoldiq : "+a);
+     //4-masala
+         while (a>=b){
+               a=a-b;
+           }
+            if (a==0)
+                System.out.println(b+" ning darajasi");
+            else System.out.println(b+" ning darajasi emas");
+     //5-masala
+           while (a>0){
+               a=a/b;
+               ++i;
+           }
+           if (a==0) System.out.println(i-1);
+     //6-masla
+        while (a>0){
+            kopaytir*=a;
+            a=a-2;
+        }
+        System.out.println(kopaytir);
+    //7-masala
+        while (i*i<=a){
+            i++;
+        }
+        System.out.println(i);
+    //8-masala
+        while (i*i<a){
+            i++;
+        }
+        System.out.println(i);
+     //9-masala
+        do {
+            i++;
+            kopaytir*=3;
+        }
+        while (3*kopaytir<=a);
+        System.out.println(i+1);
+     //10-masala
+        while (3*kopaytir<=a){
+            i++;
+            kopaytir*=3;
+        }
+        System.out.println(i);
+     //11-masala
+           while (i<=a){
+               i++;
+               yigindi+=i;
+           }
+        System.out.println("son : "+i);
+        System.out.println("yigindi : "+yigindi);
+     //12-masala
+           while (i<a){
+               i++;
+               yigindi+=i;
+           }
+        System.out.println("son : "+i);
+        System.out.println("yigindi : "+yigindi);
+     //13-masala
+           while (i<=a){
+               i= (int) (1./i);
+               yigindi+=i;
+           }
+        System.out.println("son : "+i);
+        System.out.println("yigindi : "+yigindi);
+    //14-masala
+           while (i<a){
+               i= (int) (1./i);
+               yigindi+=i;
+           }
+        System.out.println("son : "+i);
+        System.out.println("yigindi : "+yigindi);*/
+        
+    }
+}
